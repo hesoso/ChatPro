8db380fdc9557803392331968eeafdbf86e16521
jixiongbo <jixiongbo@everjiankang.com>
2025-05-31 23:48:30 +0800

    新建群聊

--- src/views/workbench/Chat/components/ConversationMode.vue
+++ src/views/workbench/Chat/components/ConversationMode.vue
@@ -1,36 +1,93 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import SearchInput from '@/components/SearchInput.vue'
+import CreateNewConversation from '@/views/workbench/Chat/components/CreateNewConversation.vue'
 
 const showSearch = ref(false)
+const showCreateNewConversation = ref(true)
 
 const handleCancel = () => {
   showSearch.value = false
 }
 
+const curModeType = ref('type1')
+const curGroupList = ref(['group1', 'group2', 'group3'])
+
+const modeList = ref([{
+  type: 'type1',
+  name: '独立'
+}, {
+  type: 'type2',
+  name: '组合'
+}, {
+  type: 'type3',
+  name: '接待'
+}])
+const groupList = ref([{
+  type: 'group1',
+  name: '好友'
+},{
+  type: 'group2',
+  name: '群'
+},{
+  type: 'group3',
+  name: '群成员'
+}])
+
+const curGroupListHanlder = (type: String) => {
+  const index = curGroupList.value.indexOf(type)
+  index > -1 ? curGroupList.value.splice(index, 1) : curGroupList.value.push(type)
+}
+
 </script>
 
 <template>
-<div class="conversation-mode">
-  <template v-if="!showSearch">
-    <el-tag type="primary" size="large" style="margin-right: 27px">
-      <div class="mode-wrap">
-        <span style="margin-right: 27px">独立[好友+群+群成员]</span>
-        <svg-icon style="font-size: 20px" name="down-blue"></svg-icon>
+  <div class="conversation-mode">
+    <template v-if="!showSearch">
+      <el-tag type="primary" size="large" style="margin-right: 27px">
+        <el-popover
+          placement="bottom"
+          :width="200"
+          trigger="click"
+        >
+          <div class="mode-select-wrap">
+            <div class="mode-section">
+              <p class="title">会话模式</p>
+              <div class="radio-wrap">
+                <span v-for="item in modeList" class="radio-item" :class="[{active: item.type === curModeType}]"
+                      @click="curModeType = item.type">{{ item.name }}</span>
+              </div>
+            </div>
+            <div class="mode-section">
+              <p class="title">组合消息</p>
+              <div class="radio-wrap">
+                <span v-for="item in groupList" class="radio-item" :class="[{active: curGroupList.includes(item.type)}]"
+                      @click="curGroupListHanlder(item.type)">{{ item.name }}</span>
+              </div>
+            </div>
+          </div>
+          <template #reference>
+            <div class="mode-wrap">
+              <span style="margin-right: 27px">独立[好友+群+群成员]</span>
+              <svg-icon style="font-size: 20px" name="down-blue"></svg-icon>
+            </div>
+          </template>
+        </el-popover>
+      </el-tag>
+      <div class="options-wrap">
+        <svg-icon name="search-gray" style="margin-right: 10px" @click="showSearch = true"></svg-icon>
+        <svg-icon name="add-gray" @click="showCreateNewConversation = true"></svg-icon>
       </div>
-    </el-tag>
-    <div class="options-wrap">
-      <svg-icon name="search-gray" style="margin-right: 10px" @click="showSearch = true"></svg-icon>
-      <svg-icon name="add-gray"></svg-icon>
-    </div>
-  </template>
-  <template v-else>
-    <div class="search-input-wrap">
-      <SearchInput></SearchInput>
-      <span class="cancel" @click="handleCancel">取消</span>
-    </div>
-  </template>
-</div>
+    </template>
+    <template v-else>
+      <div class="search-input-wrap">
+        <SearchInput></SearchInput>
+        <span class="cancel" @click="handleCancel">取消</span>
+      </div>
+    </template>
+
+    <CreateNewConversation v-if="showCreateNewConversation" v-model="showCreateNewConversation"></CreateNewConversation>
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -42,13 +99,17 @@ const handleCancel = () => {
   align-items: center;
   padding: 5px 10px;
 }
+
 .options-wrap {
   font-size: 20px;
 }
+
 .mode-wrap {
   display: flex;
   align-items: center;
+  cursor: pointer;
 }
+
 .search-input-wrap {
   display: flex;
   align-items: center;
@@ -56,6 +117,7 @@ const handleCancel = () => {
   height: 27px;
   width: 100%;
 }
+
 .cancel {
   font-weight: 400;
   font-size: 12px;
@@ -65,7 +127,46 @@ const handleCancel = () => {
   cursor: pointer;
   flex-shrink: 0;
 }
+
 .svg-icon {
   cursor: pointer;
+}
+
+.mode-select-wrap {
+  .mode-section {
+    margin-bottom: 15px;
+
+    .title {
+      font-weight: 400;
+      font-size: 14px;
+      color: #333333;
+      margin-bottom: 8px;
+    }
+
+    .radio-wrap {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+    }
+  }
+}
+
+.radio-item {
+  width: 46px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 400;
+  font-size: 10px;
+  color: #666666;
+  background: #E7E7E7;
+  border-radius: 12px 12px 12px 12px;
+  cursor: pointer;
+  &.active {
+    background: #3686FF;
+    color: #fff;
+  }
 }
 </style>
---
+++ src/views/workbench/Chat/components/CreateNewConversation.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import SearchInput from '@/components/SearchInput.vue'
+
+defineProps({
+  modelValue: Boolean
+})
+const emits = defineEmits(['update:modelValue'])
+const curIndex = ref(1)
+
+const activeName = ref('tab1')
+const handleTabChange = (tabName) => {
+
+}
+</script>
+
+<template>
+<el-dialog :model-value="modelValue" width="654" title="新建会话" @update:modelValue="emits('update:modelValue')">
+  <div class="dialog-content">
+    <div class="dialog-content-left">
+      <div class="search-input-wrap">
+        <SearchInput></SearchInput>
+      </div>
+      <div class="list-wrap scroll">
+        <div class="avatar-box" :class="[{active: item === curIndex}]" v-for="item in 20" @click="curIndex = item">
+          <img src="" alt="">
+          <div class="content">
+            <p class="title">名字29999000  <span class="text">企微</span></p>
+            <p class="text">12488888888888888</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="dialog-content-right">
+      <el-tabs v-model="activeName" @tab-change="handleTabChange" style="width:90px">
+        <el-tab-pane label="好友" name="tab1">
+        </el-tab-pane>
+        <el-tab-pane label="群" name="tab2">
+        </el-tab-pane>
+      </el-tabs>
+      <div class="search-input-wrap" style="margin: 0;width: 100%;">
+        <SearchInput></SearchInput>
+        <el-button type="primary" style="margin-left: 20px">同步好友</el-button>
+      </div>
+    </div>
+  </div>
+</el-dialog>
+</template>
+
+<style scoped lang="scss">
+.dialog-content {
+  display: flex;
+  padding-bottom: 20px;
+  .dialog-content-left {
+    width: 296px;
+    border-right: 1px solid #E5E5E5;
+  }
+  .dialog-content-right {
+    width: 357px;
+    padding: 0 30px;
+  }
+}
+.search-input-wrap {
+  width: 227px;
+  height: 30px;
+  margin: 10px auto;
+  display: flex;
+  align-items: center;
+}
+.avatar-box {
+  padding: 5px 36px 5px 36px;
+  justify-content: flex-start;
+  &.active {
+    background: #E1EDFF;
+  }
+  .content {
+    width: 100%;
+  }
+  .title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    .text {
+      margin-top: 0;
+    }
+  }
+}
+.list-wrap {
+  height: 500px;
+  overflow-y: auto;
+}
+:deep(.el-tabs__nav-wrap::after) {
+  display: none;
+}
+</style>
