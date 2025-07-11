9033aaaf825d033a23d5511a085c7dab0bc27d56
hesoso <hejianhong@everjiankang.com>
2025-07-11 16:31:54 +0800

    feat: 提交

--- electron/types/index.d.ts
+++ electron/types/index.d.ts
@@ -24,7 +24,7 @@ enum MSG_CONTENT_TYPE {
 // 消息内容类型
 type T_Msg_Content = MSG_CONTENT_TYPE
 
-// 聊天类型(1:私聊,2:群聊,3:模拟)
+// 聊天类型 (1:私聊 2:群聊 3:模拟)
 type T_Chat = '1' | '2' | '3'
 
 // 额外参数，确保多传的参数不影响主业务
@@ -32,20 +32,47 @@ interface I_Extra_Params {
     [p: string]: any
 }
 
-interface I_OnMessageParams extends I_Extra_Params {
-    wechatId: string
-    chatType: T_Chat
-    sender: string
-    receiver: string
-    senderNickname: string
-    msgId: string
-    msgTime: number
-    room: boolean
-    contentType: T_Msg_Content
-    content: string
-    draft?: string
-}
+// -- 消息表 (T_BEE_CHAT_MSG)
+// CREATE TABLE IF NOT EXISTS T_BEE_CHAT_MSG (
+//     ID                  INTEGER PRIMARY KEY AUTOINCREMENT, -- '主键'
+//     WECHAT_ID           TEXT NOT NULL, -- '微信ID'
+//     CHAT_TYPE           TEXT, -- '聊天类型(1:私聊,2:群聊,3:模拟)'
+//     SENDER              TEXT NOT NULL, -- '发送人(微信ID|群成员微信ID|模拟群成员微信ID)'
+//     SENDER_AVATAR       TEXT, -- '发送人头像'
+//     SENDER_NICKNAME     TEXT, -- '发送人昵称'
+//     RECEIVER            TEXT NOT NULL, -- '接收人(微信ID|群ID|模拟群成员微信ID)'
+//     RECEIVER_AVATAR     TEXT, -- '接收人头像'
+//     RECEIVER_NICKNAME   TEXT, -- '接收人昵称'
+//     SOURCE_ROOM_AVATAR  TEXT, -- '来源群头像'
+//     SOURCE_ROOM_NAME    TEXT, -- '来源群名称'
+//     MSG_ID              TEXT NOT NULL, -- '消息ID'
+//     CONTENT_TYPE        TEXT, -- '消息类型(@枚举)'
+//     CONTENT             TEXT, -- '消息内容'
+//     REVOKE_FLAG         TEXT, -- '撤回标识(1:是,0:否)'
+//     CLIENT_FLAG         TEXT, -- '客户端消息标识(1:是,0:否)' 默认传1
+//     SEND_USER_ID        TEXT, -- '发送用户ID'
+//     SEND_USER_NICKNAME  TEXT, -- '发送用户昵称'
+//     CREATE_BY           TEXT, -- '创建人'
+//     CREATE_TIME         INTEGER, -- '创建时间'
+//     UPDATE_BY           TEXT, -- '修改人'
+//     UPDATE_TIME         INTEGER,  -- '修改时间'
+//     QUOTE_MSG_ID        TEXT, -- '引用消息ID'
+//     SEND_FLAG           TEXT -- '消息方向(0:接收,1:发送)'
+// );
 
+interface I_OnMessageParams extends I_Extra_Params {
+    wechatId: string // 微信ID
+    chatType: T_Chat
+    sender: string // 发送人(微信ID|群成员微信ID|模拟群成员微信ID)
+    receiver: string // 接收人(微信ID|群ID|模拟群成员微信ID)
+    senderNickname: string // 发送人昵称
+    msgId: string // 消息ID
+    msgTime: number // 消息创建时间
+    room: boolean // 是否群聊
+    contentType: T_Msg_Content
+    content: string // 消息内容
+    draft?: string // 草稿内容
+}
 
 interface I_GetMessagesParams extends I_Extra_Params {
     wechatId: string
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-
-onMounted(async () => {
-  window.databaseApi.onMessage({
-      "wechatId": '1008611',
-      "chatType": '1',
-      "draft": '213',
-      "sender": "7881302256961285",
-      "receiver": "10737336402983720",
-      "senderNickname": "那年十八",
-      "msgId": "6038979939951729151",
-      "msgTime": 1751506557,
-      "room": true,
-      "contentType": 3,
-      "content": "hello bee"
+window.databaseApi.onMessage({
+        "wechatId": '1008612111',
+        "chatType": '1',
+        "draft": '213',
+        "sender": "7881302256961285",
+        "receiver": "10737336402983720",
+        "senderNickname": "那年十八",
+        "msgId": "60389799399517291521112",
+        "msgTime": 1751506557,
+        "room": true,
+        "contentType": 3,
+        "content": "hello bee"
     }
   )
 
-  const messages = await window.databaseApi.getMessages({
-    "wechatId": '1008611',
-    "convoId": '7881302256961285',
-    "chatType": '1',
-    options: {}
-  })
-
-  console.log('从数据中取得的消息messages有：', messages.data)
+const messages = await window.databaseApi.getMessages({
+        "wechatId": '1008612111',
+        "convoId": '7881302256961285',
+        "chatType": '1',
+        options: {}
 })
 
 </script>
